e146b08c213cb30d9bd251b32862d6a0cf25c793
William Johnston <will@johnstonclan.net>
2013-06-03 21:28:37 -0500

    Simplify find function.

--- game.js
+++ game.js
@@ -8,16 +8,9 @@
     Array.prototype.find = function(searchStr) {
         var returnArray = false;
         for (var i=0; i<this.length; i++) {
-            if (typeof(searchStr) == 'function') {
-                if (searchStr.test(this[i])) {
-                    if (!returnArray) { returnArray = [] }
-                    returnArray.push(i);
-                }
-            } else {
-                if (this[i]===searchStr) {
-                    if (!returnArray) { returnArray = [] }
-                    returnArray.push(i);
-                }
+            if (this[i] === searchStr) {
+                if (!returnArray) { returnArray = [] }
+                returnArray.push(i);
             }
         }
         return returnArray;
