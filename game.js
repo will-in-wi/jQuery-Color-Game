562750f43ef8911b5c0b91bb2a1bd7af4034eb26
William Johnston <will@johnstonclan.net>
2013-06-03 21:01:23 -0500

    Generate game html.

--- game.js
+++ game.js
@@ -25,11 +25,46 @@
 
 
     $.fn.colorGame = function(options) {
+
+        // Set up options.
         var defaults = {
             noOfColors : 6,
             size       : 12
         }
         options = $.extend({}, defaults, options);
+
+
+        // Initialize gameboard.
+        // Counter, grid, color select.
+        var counter = $('<div />')
+            .attr('id', 'counter')
+            .html('0 of 22 moves'); // Compute this from size.
+
+        var grid = $('<div />')
+            .attr('id', 'grid');
+
+        var makeControls = function() {
+            var colorSelect = $('<table />')
+                .attr('id', 'color-select');
+
+            var tr = $('<tr />');
+
+            for (var i = 0; i < options.noOfColors; i++) {
+                var td = $('<td />')
+                    .attr('id', 'pick-' + i)
+                    .addClass('color-' + i);
+                tr.append(td);
+            };
+
+            colorSelect.append(tr);
+
+            return colorSelect;
+        }
+
+        this.append(counter);
+        this.append(grid);
+        this.append(makeControls());
+
 
         var whichcolor = function(cell) {
             var cell = $('#' + cell);
@@ -42,17 +77,17 @@
             throw 'Cell does not have color class specified.';
         }
 
-        var makegrid = function(size) {
+        var makegrid = function() {
             var grid = $('<table/>');
 
             var cell = 0;
 
             var i = 1;
-            for (i=1;i<=size;i++) {
+            for (i=1;i<=options.size;i++) {
                 var tr = $('<tr />');
 
                 var j = 1;
-                for (j=1;j<=size;j++) {
+                for (j=1;j<=options.size;j++) {
                     var color = Math.floor(Math.random()*(options.noOfColors));
                     var td = $('<td />')
                         .attr('id', 'cell-' + cell)
@@ -120,7 +155,7 @@
         var currentStep = 0;
 
         $("#easy").click(function(){
-            $("#grid").html(makegrid(options.size));
+            $("#grid").html(makegrid());
             $("#start").hide();
             $("#game").show();
         });
